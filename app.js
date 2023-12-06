350f7d07ee5071bf10e3a26064ffc6dbb59c4b4c
Fahad Khan <fk4444497@gmail.com>
2023-12-06 11:49:43 +0500

    Memory Game

---
+++ app.js
@@ -0,0 +1,8 @@
+const emojis = ["😍","😍","💖","💖","😫","😫","🤣","🤣","😎","😎",
+"😡","😡","👍","👍","😊","😊",];
+var shuf_emojis = emojis.sort(() => (Math,random() > .5) ? 2 : -1);
+for ( var i =0; i<emojis.length; i++){
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+    document.querySelector('.game') .appendChild(box);}
